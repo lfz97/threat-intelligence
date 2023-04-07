16011cc5c8de21a6835682c786fc596fb9c714b3
Skactor <sk4ct0r@gmail.com>
2023-04-07 16:26:13 +0800

    remove tenable

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from src import config
 from src.utils import log
 from src.utils.sqlite import SqliteSDBC
 
-from src.crawler.tenable import Tenable
 from src.crawler.oscs import OSCS
 from src.crawler.cert360 import Cert360
-# from src.crawler.nsfocus import NsFocus
 from src.crawler.qianxin import QiAnXin
 from src.crawler.redqueen import RedQueen
 from src.crawler.vas import Vas
@@ -54,13 +52,11 @@ def main(
         all_cves = {}
         srcs = [
             Cert360(),
-            # NsFocus(),
             QiAnXin(),
             RedQueen(),
             AVDCrawler(),
             Vas(),
             OSCS(),
-            Tenable(),
         ]
         for src in srcs:
             cve_list = src.cves()
